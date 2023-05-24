9a3fc223c3dce88dd5a7ff719daf68490730fb28
olga-laboza <olga.laboza@gmail.com>
2023-05-24 14:23:02 +0300

    the first homework FE

---
+++ homework/hw_1/script/script.js
@@ -0,0 +1,24 @@
+let name = 'Olga'; // type string 
+            let age = '17';
+
+            const variable2 = true; // type boolean
+            var variable3 = 2; //globa var, number
+            let variable4 = 8;
+
+            //number, boolean, null, string, undefined, array
+
+            // if (age >= 18) {
+            //     let h1 = document.createElement('h1');
+            //     h1.append(name);
+            //     document.getElementsByTagName('body')[0].appendChild(h1);
+            // }
+
+            // if (age < 18 && age == 17) {
+            //     alert('Скоро ты будешь совершеннолетний');
+            // }
+
+
+            //console.log('variable1', variable1); 
+            console.log('variable3+variable4', variable3+variable4);
+
+            // +   -   /  *   %  ==  >  <  =   <=   >=  ++  --   +=  != 
